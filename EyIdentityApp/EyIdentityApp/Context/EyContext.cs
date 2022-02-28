785cd0b1bc5293fbcf650d17169abb9a72c149f5
E Y <eyup.yigit@outlook.com.tr>
2022-02-28 11:05:58 +0300

    Identity Table Created

---
+++ EyIdentityApp/EyIdentityApp/Context/EyContext.cs
@@ -0,0 +1,14 @@
+﻿using EyIdentityApp.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EyIdentityApp.Context
+{
+    public class EyContext :IdentityDbContext<AppUser,AppRole,int>
+    {
+        public EyContext(DbContextOptions<EyContext> options):base(options)
+        {
+             
+        }
+    }
+}
