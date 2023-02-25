04b00213cfa9b5d5cae49e40222701dd2ca7e50a
eyup.yigit <eyup.yigit@DESKTOP-QCN50RO>
2023-02-25 10:59:22 +0300

    program.cs konfigurasyonları yapıldı

--- EyIdentityApp/EyCoreYediIdentity/Program.cs
+++ EyIdentityApp/EyCoreYediIdentity/Program.cs
@@ -1,7 +1,43 @@
+using EyCoreYediIdentity.CustomDescriber;
+using EyCoreYediIdentity.DbContext;
+using EyCoreYediIdentity.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentity<AppUser, AppRole>(opt =>
+{
+    //opt.Password.RequireDigit = false;
+    //opt.Password.RequiredLength = 4;
+    //opt.Password.RequireLowercase = false;
+    //opt.Password.RequireUppercase = false;
+    //opt.Password.RequireNonAlphanumeric = false;
+    //opt.SignIn.RequireConfirmedEmail = false;
+    opt.Lockout.MaxFailedAccessAttempts = 3;
+}).AddErrorDescriber<CustomErrorDescriber>().AddEntityFrameworkStores<Context>();
+
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    opt.Cookie.HttpOnly = true;
+    opt.Cookie.SameSite = SameSiteMode.Strict;
+    opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    opt.Cookie.Name = "EyCookie";
+    opt.ExpireTimeSpan = TimeSpan.FromDays(25);
+    opt.LoginPath = new PathString("/Home/SignIn");
+    opt.LogoutPath = new PathString("/Home/Logout");
+    opt.AccessDeniedPath = new PathString("/Home/AccessDenied");
+});
+
+builder.Services.AddDbContext<Context>(opt =>
+{
+    opt.UseSqlServer("server=localhost; database=EyIdentityDb; integrated security=true;");
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
@@ -11,13 +47,21 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
-
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+    RequestPath = "/node_modules",
+});
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
 
 app.Run();
