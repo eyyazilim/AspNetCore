2513465a84ad77dba16b295fb2ea709ef9bc8aa3
eyup.yigit <eyup.yigit@DESKTOP-QCN50RO>
2023-02-25 11:07:20 +0300

    Desing işlemleri ve back end işlemleri tamamlandı. Db bağlantısı yapılım test edilecektir.

--- EyIdentityApp/EyCoreYediIdentity/Program.cs
+++ EyIdentityApp/EyCoreYediIdentity/Program.cs
@@ -47,11 +47,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
-app.UseStaticFiles(new StaticFileOptions()
-{
-    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
-    RequestPath = "/node_modules",
-});
+//app.UseStaticFiles(new StaticFileOptions()
+//{
+//    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "node_modules")),
+//    RequestPath = "/node_modules",
+//});
 app.UseRouting();
 
 app.UseAuthentication();
