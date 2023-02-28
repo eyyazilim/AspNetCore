056e36b4bfebca43f49cc563962196cba22c1561
eyup.yigit <eyup.yigit@DESKTOP-QCN50RO>
2023-02-28 09:45:46 +0300

    class libray

---
+++ EyIdentityApp/Core/Context/EyContext.cs
@@ -0,0 +1,14 @@
+﻿using Core.Etities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Context
+{
+    public class EyContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        public EyContext(DbContextOptions<EyContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ EyIdentityApp/Core/Etities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Core.Etities
+{
+    public class AppUser : IdentityRole<int>
+    {
+        public string ImagePath { get; set; }
+    }
+}
